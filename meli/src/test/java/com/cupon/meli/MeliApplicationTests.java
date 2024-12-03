a4b6f76134fde48434a766a51bcd01295dcdd24a
Luis Carlos <sistemas.lucar@gmail.com>
2024-12-03 14:01:29 -0500

    cuponMeli

---
+++ meli/src/test/java/com/cupon/meli/MeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupon.meli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
